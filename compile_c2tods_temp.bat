aeab497ce5beb95cf2cfe38c961f14d6a87cd2ec
NeoDement <jburnett3d@gmail.com>
2024-07-16 11:25:33 +0100

    temp compile instructions.

---
+++ compile_c2tods_temp.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\6YFAF\Documents\GitHub\C2toDS-Refresh\compile_temp"
+
+E:\2TB_M2_DRIVE_BACKUP\Users\jburn\Documents\Jagent\PRAYBuilder.exe --compress C:\Users\6YFAF\Documents\GitHub\C2toDS-Refresh\compile_temp\C2toDS_Refresh_Part1.txt
+
+E:\2TB_M2_DRIVE_BACKUP\Users\jburn\Documents\Jagent\PRAYBuilder.exe --compress C:\Users\6YFAF\Documents\GitHub\C2toDS-Refresh\compile_temp\C2toDS_Refresh_Part2.txt
